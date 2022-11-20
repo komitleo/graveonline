c1f883dd5bdbb6b421f095f3eb9ca541386d06af
Komitleo-PC <jacek@komitleo.pl>
2022-11-20 14:44:59 +0100

    Initial commit

---
+++ src/main/java/pl/komitleo/graveonline/GraveonlineApplication.java
@@ -0,0 +1,13 @@
+package pl.komitleo.graveonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraveonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraveonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/komitleo/graveonline/GraveonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.komitleo.graveonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraveonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
